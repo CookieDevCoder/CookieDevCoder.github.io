36c23985de989cabdc57d611b0a2c023e34e416e
Cookie <122497552+CookieDevCoder@users.noreply.github.com>
2025-03-24 09:01:30 -0700

    Added Terminal input

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
 // Keypresses 
-currentInput = '';
+var currentInput = "";
 
-terminalLoc = "BrianAyala@portfolio:~$"
-const CMDLine = document.getElementById("CommandLine");
+const terminalDir = "BrianAyala@portfolio:~$"
+var CMDOutput = document.getElementById("output");
 
 document.addEventListener('keydown', function(event) {
-    // The 'event' object contains information about the key press
     console.log('Key pressed:', event.key);
+    if (event.key === " ") {
+        console.log('Key Space pressed*');
+    }
+    var CMDLine = document.getElementById("cmdline");
 
-    currentInput += event.key;
+    if (event.key === "Enter") {
+        // Write input to CMDOutput
+        var CMDOutput = document.getElementById("output");
+        CMDOutput.innerText += terminalDir + currentInput + '\n';
+        currentInput = "";
 
-    //CMDLine.textContent = terminalLoc + " " + currentInput; 
+        window.scrollTo(0, document.body.scrollHeight)
+    }
+    else if (event.key === "Backspace") {
+        // Delete last character
+        currentInput = currentInput.slice(0, -1);
+    }
+    else if (event.key.length <= 1) {
+        currentInput += event.key;
+    }
+    CMDLine.textContent = terminalDir + currentInput; 
+    CMDLine.style.width = (terminalDir + currentInput).length + "ch"
   });
