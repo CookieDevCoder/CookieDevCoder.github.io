16c17c33e6913f32d0301fbcbefbb3c90d246855
Cookie <122497552+CookieDevCoder@users.noreply.github.com>
2025-03-24 09:05:54 -0700

    Cleaned up unused code and added comments

--- main.js
+++ main.js
@@ -2,13 +2,9 @@
 var currentInput = "";
 
 const terminalDir = "BrianAyala@portfolio:~$"
-var CMDOutput = document.getElementById("output");
 
 document.addEventListener('keydown', function(event) {
     console.log('Key pressed:', event.key);
-    if (event.key === " ") {
-        console.log('Key Space pressed*');
-    }
     var CMDLine = document.getElementById("cmdline");
 
     if (event.key === "Enter") {
@@ -17,6 +13,7 @@ document.addEventListener('keydown', function(event) {
         CMDOutput.innerText += terminalDir + currentInput + '\n';
         currentInput = "";
 
+        // scroll to bottom on enter
         window.scrollTo(0, document.body.scrollHeight)
     }
     else if (event.key === "Backspace") {
@@ -24,8 +21,10 @@ document.addEventListener('keydown', function(event) {
         currentInput = currentInput.slice(0, -1);
     }
     else if (event.key.length <= 1) {
+        // Only accepts char inputs (excluding esc, alt, ctrl, ect.s)
         currentInput += event.key;
     }
+    // Update Command Line text
     CMDLine.textContent = terminalDir + currentInput; 
     CMDLine.style.width = (terminalDir + currentInput).length + "ch"
   });
